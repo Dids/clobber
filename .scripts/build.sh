208ff0548219f6ac6f0f19c1ae4c39fb3d5f3619
Pauli Jokela <pauli.jokela@didstopia.com>
2018-08-13 11:19:51 +0300

    Added a simple build script [skip ci]

---
+++ .scripts/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Setup error handling
+set -o errexit   # Exit when a command fails (set -e)
+set -o nounset   # Exit when using undeclared variables (set -u)
+set -o pipefail  # Exit when piping fails
+set -o xtrace    # Enable debugging (set -x)
+
+# Setup overrideable arguments
+VERSION=${1:-0.0.1}
+OUTPUT=${2:-clobber}
+
+# Build the application
+go build -ldflags "-X main.Version=${VERSION}" -o ${OUTPUT}
