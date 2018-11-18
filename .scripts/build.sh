a7f8af8b0b49a9ce08cc9c9e3e5a815d45c3918a
Pauli Jokela <pauli.jokela@didstopia.com>
2018-11-18 08:28:55 +0200

    Fixing Travis tests

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -18,8 +18,8 @@ ls $GOPATH/src
 ls $GOPATH/bin
 
 # Prepare the Packr build command (this is a workaround for Homebrew)
-#BUILD_CMD="go run $GOPATH/src/github.com/gobuffalo/packr/v2/packr2/main.go"
-BUILD_CMD="$GOPATH/bin/packr2"
+BUILD_CMD="go run $GOPATH/src/github.com/gobuffalo/packr/v2/packr2/main.go"
+#BUILD_CMD="$GOPATH/bin/packr2"
 
 # Build the application
 #go build -ldflags "-X main.Version=${VERSION}" -o ${OUTPUT}
