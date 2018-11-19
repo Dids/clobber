cb53390b9b52adb60f280579f27e076a3aaded44
Pauli Jokela <pauli.jokela@didstopia.com>
2018-11-19 09:27:08 +0200

    Fixing Travis

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -12,13 +12,13 @@ OUTPUT=${2:-clobber}
 
 # Define necessary paths
 GOBIN=$GOPATH/bin
-REAL_CWD=$(pwd)
-CWD=${TRAVIS_BUILD_DIR:-$REAL_CWD}
+#REAL_CWD=$(pwd)
+#CWD=${TRAVIS_BUILD_DIR:-$REAL_CWD}
 
 # Remove intermediate files
-rm -fr clobber \
-       packrd
-find . -type f -name '*-packr.go' -delete
+#rm -fr clobber \
+#       packrd
+#find . -type f -name '*-packr.go' -delete
 
 # Make sure Packr is installed
 go get -u github.com/gobuffalo/packr/v2/packr2
@@ -27,9 +27,18 @@ go get -u github.com/gobuffalo/packr/v2/packr2
 #BUILD_CMD="go run $GOPATH/src/github.com/gobuffalo/packr/v2/packr2/main.go"
 
 # Switch to the correct directory
-cd $GOPATH/src/github.com/Dids/clobber
+#cd $GOPATH/src/github.com/Dids/clobber
+
+# Clean the intermediate files
+$GOBIN/packr2 clean
+
+## TODO: Why would we need to do this?
+rm -fr vendor
 
 # Build the application
 #$BUILD_CMD build -ldflags "-X main.Version=${VERSION}" -o ${OUTPUT} .
 $GOBIN/packr2 build -ldflags "-X main.Version=${VERSION}" -o ${OUTPUT} .
 #$GOBIN/packr2 build -ldflags "-X main.Version=${VERSION}" -o ${OUTPUT} ${CWD}
+
+# Clean the intermediate files
+$GOBIN/packr2 clean
